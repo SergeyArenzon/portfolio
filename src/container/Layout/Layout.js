284c4715523d9d839cdf6bc2606e043aa120a97b
SergeyArenzon <sergeyr1991@gmail.com>
2021-01-28 05:58:02 +0200

    add typing effect

--- src/container/Layout/Layout.js
+++ src/container/Layout/Layout.js
@@ -1,25 +1,34 @@
 import React from "react";
 import classes from "./Layout.module.css";
 import Typical from "react-typical";
-import Logo from '../../components/Logo/Logo';
+import Logo from "../../components/Logo/Logo";
+import Typewriter from "typewriter-effect";
 
 export default function Layout() {
     return (
         <div className={classes.Layout}>
             <nav>
-                <Logo/>
+                <Logo />
             </nav>
             <div className={classes.Picture}>
                 <div>
                     {/* <Typical
                         loop={Infinity}
                         wrapper="div"
-                        steps={["Hello my name is Sergey and I am a Web Developer", 1000 , '']}
+                        steps={["Hello my name is Sergey and I am a Web Developer", 3000 , '']}
                     /> */}
-                    
-
+                    <Typewriter
+                        onInit={(typewriter) => {
+                            typewriter
+                                .typeString(
+                                    "Hello my name is Sergey and I am a Web Developer"
+                                )
+                                .pauseFor(2000)
+                                // .deleteAll()
+                                .start();
+                        }}
+                    />
                 </div>
-                
             </div>
         </div>
     );
