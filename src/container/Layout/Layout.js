ba7f31aaef21afc7ffa975046e9243cc8994a273
SergeyArenzon <sergeyr1991@gmail.com>
2021-02-01 22:35:46 +0200

    fixing css

--- src/container/Layout/Layout.js
+++ src/container/Layout/Layout.js
@@ -1,72 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import classes from "./Layout.module.css";
-import Typewriter from "typewriter-effect";
-import Navbar from '../../components/Navbar/Navbar';
-import Picture from '../../components/Picture/Picture';
+import Navbar from "../../components/Navbar/Navbar";
+import Picture from "../../components/Picture/Picture";
 
 // =========================
 // Main scrolling background
 // =========================
 
-
 export default function Layout() {
-    // const [showText, setShowText] = useState(false);
-
-
-    // //  Delaying the seconde typing effect
-    // useEffect(() => {
-    //     setTimeout(function () {
-    //         setShowText(true);
-    //     }, 2000);
-    // }, []);
-
-    // //  ======================
-    // //  Typing effect controls
-    // //  ======================
-
-
-    // // Delayed typing effect
-    // let text = null;
-    // if (showText) {
-    //     text = (
-    //         <div className={classes.TypeSec}>
-    //             <Typewriter
-    //                 className={classes.TypeSec}
-    //                 options={{
-    //                     strings: [
-    //                         "FULLSTACK Developer.",
-    //                         "FRONTEND Developer.",
-    //                         "BACKEND Developer.",
-    //                     ],
-    //                     autoStart: true,
-    //                     loop: true,
-    //                     delay: 20,
-    //                 }}
-    //             />
-    //         </div>
-    //     );
-    // }
-
-
-    // // First kicked typing effect
-    // const staticText = (
-    //     <Typewriter
-    //         options={{
-    //             strings: ["Hi my name is Sergey Arenzon and I'm a"],
-    //             autoStart: true,
-    //             loop: false,
-    //             pauseFor: 1000000000000000000,
-    //             delay: 40,
-    //         }}
-    //     />
-    // );
-
-
-
     return (
         <div className={classes.Layout}>
-            <Navbar/>
-            <Picture/>
+            <Navbar />
+            <Picture />
+            <div className={classes.c}></div>
         </div>
     );
 }
