3e1c798e1e5e75d155b82fb5d2ef30f81a4788ae
SergeyArenzon <sergeyr1991@gmail.com>
2021-02-02 01:07:16 +0200

    add video background

--- src/container/Layout/Layout.js
+++ src/container/Layout/Layout.js
@@ -2,6 +2,7 @@ import React from "react";
 import classes from "./Layout.module.css";
 import Navbar from "../../components/Navbar/Navbar";
 import Picture from "../../components/Picture/Picture";
+import video from "../../assets/video/background.mp4";
 
 // =========================
 // Main scrolling background
@@ -10,6 +11,9 @@ import Picture from "../../components/Picture/Picture";
 export default function Layout() {
     return (
         <div className={classes.Layout}>
+            <video autoPlay loop muted>
+                <source src={video} type="video/mp4" />
+            </video>
             <Navbar />
             <Picture />
             <div className={classes.c}>test div</div>
