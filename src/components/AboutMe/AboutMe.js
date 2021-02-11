6ec5adec6d35d28c5f046adc36f9ec09ded2ff8f
SergeyArenzon <sergeyr1991@gmail.com>
2021-02-11 04:48:22 +0200

    fix aboutme

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import classes from './AboutMe.module.css';
+import React from "react";
+import classes from "./AboutMe.module.css";
 
 export default function AboutMe() {
-
-    
     return (
         <div className={classes.AboutMe}>
-            <div className={classes.Profile}></div>
-            <h1>ABOUT <label>ME</label></h1>
-            <p>lorema asf sdf gfdfgslma ;sdfk;a lsds;gi js;dlgm</p>
+            <div className={classes.LeftSide}>
+                <div className={classes.Profile}></div>
+            </div>
+            <div className={classes.RightSide}>
+                <h1>
+                    ABOUT <label>ME</label>
+                </h1>
+                <p>lorema asf sdf gfdfgslma ;sdfk;a lsds;gi js;dlgm</p>
+
+            </div>
+
         </div>
-    )
+    );
 }
