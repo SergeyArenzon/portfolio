d5a7316312f9142ec08303e62aacb4965ec09b74
SergeyArenzon <sergeyr1991@gmail.com>
2021-02-11 05:13:34 +0200

    fixes

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -5,11 +5,13 @@ export default function AboutMe() {
     return (
         <div className={classes.AboutMe}>
             <div className={classes.LeftSide}>
+                <h1>About</h1>
                 <div className={classes.Profile}></div>
+                
             </div>
             <div className={classes.RightSide}>
                 <h1>
-                    ABOUT <label>ME</label>
+                    ME
                 </h1>
                 <p>lorema asf sdf gfdfgslma ;sdfk;a lsds;gi js;dlgm</p>
 
