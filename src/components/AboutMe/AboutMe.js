9f4cd5ac6cd3b91605cb89cf2e2729ff7bd7403e
SergeyArenzon <sergeyr1991@gmail.com>
2021-02-16 03:18:04 +0200

    add tail to aboutme left

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -5,18 +5,17 @@ export default function AboutMe() {
     return (
         <div className={classes.AboutMe}>
             <div className={classes.LeftSide}>
-                <h1>About</h1>
+                <h1 className={classes.AboutText}>
+                    About<h1 className={classes.MeText}>ME</h1>
+                </h1>
                 <div className={classes.Profile}></div>
-                
+                <div className={classes.Tail}>
+                    <div></div>
+                </div>
             </div>
             <div className={classes.RightSide}>
-                <h1>
-                    ME
-                </h1>
                 <p>lorema asf sdf gfdfgslma ;sdfk;a lsds;gi js;dlgm</p>
-
             </div>
-
         </div>
     );
 }
