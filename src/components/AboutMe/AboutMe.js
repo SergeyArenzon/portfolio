58dbe4d11ae311ae528226a7c4a577e6bcab5156
SergeyArenzon <sergeyr1991@gmail.com>
2021-02-17 22:36:24 +0200

    add social icons

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -9,6 +9,21 @@ export default function AboutMe() {
                     About<h1 className={classes.MeText}>ME</h1>
                 </h1>
                 <div className={classes.Profile}></div>
+                <ul>
+                    <li>
+                        <i className="fab fa-linkedin fa-2x"></i>
+                    </li>
+
+                    <li>
+                        <i class="fab fa-github-square fa-2x"></i>
+                    </li>
+                    <li>
+                    <i class="fas fa-envelope-square fa-2x"></i>
+                    </li>
+                    <li>
+                    <i class="fab fa-facebook-square fa-2x"></i>
+                    </li>
+                </ul>
                 <div className={classes.Tail}>
                     <div></div>
                 </div>
