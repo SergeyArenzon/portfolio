e1c27aa919a88ae3ce7c3f5a48423e5a2e7c97be
SergeyArenzon <sergeyr1991@gmail.com>
2021-02-22 17:05:10 +0200

    add Clouds

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from "./AboutMe.module.css";
+import Clouds from '../UI/Clouds/Clouds';
 
 export default function AboutMe() {
     const socialIcons = (
@@ -45,33 +46,37 @@ export default function AboutMe() {
     );
 
     return (
-        <div className={classes.AboutMe}>
-            <div className={classes.LeftSide}>
-                <h1 className={classes.AboutText}>
-                    About<h1 className={classes.MeText}>ME</h1>
-                </h1>
+        <div><div className={classes.AboutMe}>
+        <div className={classes.LeftSide}>
+            <h1 className={classes.AboutText}>
+                About<h1 className={classes.MeText}>ME</h1>
+            </h1>
 
-                <div className={classes.Profile}></div>
-                {socialIcons}
+            <div className={classes.Profile}></div>
+            {socialIcons}
 
-                <div className={classes.Tail}>
-                    <div></div>
-                </div>
-            </div>
-            <div className={classes.RightSide}>
-                <p className={classes.TextP}>
-                    I'am BS.c graduate of{" "}
-                    <strong>Computer Science & Mathematics</strong> and web
-                    fullstack developer course.<br></br> Experienced with the newest technologies like{" "}
-                    <strong>
-                        React, Redux, React Hooks, NodeJS, ExpressJS & MongoDB
-                    </strong>
-                    .
-                    <br></br>
-                    Aquired good knowledge in <strong>JavaScript & CSS</strong> with very good interpersonal skills and a part time <strong>GAMER <i className="fas fa-gamepad"></i></strong>
-                </p>
-                <div className={classes.Text}></div>
+            <div className={classes.Tail}>
+                <div></div>
             </div>
         </div>
+        <div className={classes.RightSide}>
+            <p className={classes.TextP}>
+                I'am BS.c graduate of{" "}
+                <strong>Computer Science & Mathematics</strong> and web
+                fullstack developer course.<br></br> Experienced with the newest technologies like{" "}
+                <strong>
+                    React, Redux, React Hooks, NodeJS, ExpressJS & MongoDB
+                </strong>
+                .
+                <br></br>
+                Aquired good knowledge in <strong>JavaScript & CSS</strong> with very good interpersonal skills and a part time <strong>GAMER <i className="fas fa-gamepad"></i></strong>
+            </p>
+            {/* <div className={classes.Text}></div> */}
+        </div>
+    
+    </div>
+    <Clouds/>
+    </div>
+        
     );
 }
