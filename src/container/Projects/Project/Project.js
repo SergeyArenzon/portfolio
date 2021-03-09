08ea8ea373037f97ded1ea7e41f248d95dae7c86
SergeyArenzon <sergeyr1991@gmail.com>
2021-03-09 18:07:56 +0200

    fix project pics slide

--- src/container/Projects/Project/Project.js
+++ src/container/Projects/Project/Project.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import classes from "./Project.module.css";
 import VisibilitySensor from "react-visibility-sensor";
+import pic from "./../../../assets/images/profile.jpeg";
 
 export default function Project(props) {
     const [show, setShow] = useState(false);
@@ -13,24 +14,28 @@ export default function Project(props) {
 
     let pictureClasses = null;
     let projectClasses = null;
-
-
     if (props.colored) {
         pictureClasses = classes.HiddenPictureLeft;
         projectClasses = classes.Project;
-        
-    }else{
+    } else {
         pictureClasses = classes.HiddenPictureRight;
         projectClasses = [classes.Project, classes.ProjectReverse].join(" ");
     }
 
-
-    
-    
     return (
-        <VisibilitySensor partialVisibility onChange={onChange}>
+        <VisibilitySensor partialVisibility offset={{bottom:300}} onChange={onChange}>
             <div className={projectClasses}>
-                <div className={[pictureClasses, show ? classes.ShowenPicture : null].join(' ')}></div>
+                <div className={classes.PictureContainer}>
+                    <img
+                        src={pic}
+                        alt={""}
+                        className={[
+                            pictureClasses,
+                            show ?  props.colored ? [classes.ShowenPicture, classes.RotateLeft].join(' ') : [classes.ShowenPicture, classes.RotateRight].join(' ') : null,
+                        ].join(" ")}
+                    />
+                </div>
+
                 <div class={classes.Description}>
                     <p>{props.projectData.description}</p>
                 </div>
