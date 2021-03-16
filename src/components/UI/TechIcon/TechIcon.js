38be5d9db6d2496dd3bc5546fcd5b2ee9ab09113
SergeyArenzon <sergeyr1991@gmail.com>
2021-03-17 00:53:44 +0200

    fix project height

--- src/components/UI/TechIcon/TechIcon.js
+++ src/components/UI/TechIcon/TechIcon.js
@@ -2,14 +2,63 @@ import React from "react";
 import classes from "./TechIcon.module.css";
 
 export default function TechIcon(props) {
+    let hintName = null;
 
- 
+    switch (props.iconName) {
+        case "express":
+            hintName = "Express";
+            break;
+        case "firebase":
+            hintName = "Firebase";
+            break;
+        case "heroku":
+            hintName = "Heroku";
+            break;
+        case "java":
+            hintName = "Java";
+            break;
+        case "javascript":
+            hintName = "JavaScript";
+            break;
+        case "jwt":
+            hintName = "JWT";
+            break;
+        case "mongodb":
+            hintName = "Mongo";
+            break;
+        case "netlify":
+            hintName = "Netlify";
+            break;
+        case "nodejs":
+            hintName = "NodeJS";
+            break;
+        case "python":
+            hintName = "Python";
+            break;
+        case "react":
+            hintName = "React";
+            break;
+        case "redux":
+            hintName = "Redux";
+            break;
+        case "twitter":
+            hintName = "Twitter";
+            break;
+        default:
+            break;
+    }
 
     return (
         <div className={classes.TechIcon}>
-            <div>{props.iconName}<div></div></div>
+            <div>
+                {hintName}
+                <div></div>
+            </div>
             <img
-                src={require(`../../../assets/icons/${props.iconName}.svg`).default}
+                src={
+                    require(`../../../assets/icons/${props.iconName}.svg`)
+                        .default
+                }
                 alt="icons"
             />
         </div>
