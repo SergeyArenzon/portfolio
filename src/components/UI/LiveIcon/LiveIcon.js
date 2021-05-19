17503569469732b125d993ba3b890b3f91c61e4d
SergeyArenzon <sergeyr1991@gmail.com>
2021-05-19 14:49:20 +0300

    fix project btns

--- src/components/UI/LiveIcon/LiveIcon.js
+++ src/components/UI/LiveIcon/LiveIcon.js
@@ -1,10 +1,20 @@
-import React from "react";
-import classes from "./LiveIcon.module.css";
+import React from 'react';
+import classes from './LiveIcon.module.css';
 
 export default function LiveIcon(props) {
     return (
-        <a className={[classes.LiveIcon, props.colored && classes.WhiteBtn].join(' ')} href={props.url} target="_blank" rel="noreferrer">
-            Live Demo
-        </a>
+        <div className={classes.LiveIconContainer}>
+            <a
+                className={[
+                    classes.LiveIcon,
+                    props.colored && classes.WhiteBtn,
+                ].join(' ')}
+                href={props.url}
+                target="_blank"
+                rel="noreferrer"
+            >
+                Live Demo
+            </a>
+        </div>
     );
 }
