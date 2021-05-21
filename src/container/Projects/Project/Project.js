5b9f35e677e562ba0f4c3d0ba060a1def458913b
SergeyArenzon <sergeyr1991@gmail.com>
2021-05-21 05:12:42 +0300

    fix project pic slide effect in mobile

--- src/container/Projects/Project/Project.js
+++ src/container/Projects/Project/Project.js
@@ -82,10 +82,18 @@ export default React.memo(function Project(props) {
                 </VisibilitySensor>
 
                 {/* PROJECT IMAGE */}
-                <VisibilitySensor partialVisibility onChange={onChange}>
-                    <div className={classes.ImageContainer} >
+                <VisibilitySensor partialVisibility offset={{bottom:400}} onChange={onChange}>
+                    <div className={classes.ImageContainer}>
                         <img
-                            style={props.projectData.name ==='MyAppartment' ? {height:'400px',width: 'auto', transform:'translateY(0)'} : null}
+                            style={
+                                props.projectData.name === 'MyAppartment'
+                                    ? {
+                                          height: '400px',
+                                          width: 'auto',
+                                          transform: 'translateY(0)',
+                                      }
+                                    : null
+                            }
                             src={props.image}
                             alt={''}
                             className={
@@ -100,7 +108,9 @@ export default React.memo(function Project(props) {
                             }
                         />
                         <svg
-                            className={`${classes.Bulb} ${projectTitleColored && classes.WhiteBulb}`}
+                            className={`${classes.Bulb} ${
+                                projectTitleColored && classes.WhiteBulb
+                            }`}
                             id="visual"
                             viewBox="0 0 500 500"
                             width="500"
@@ -111,7 +121,11 @@ export default React.memo(function Project(props) {
                             <g transform="translate(229.25185259310652 244.3487189088508)">
                                 <path
                                     d="M130.1 -216.6C158.8 -208.7 165.6 -154.1 178.6 -110.2C191.6 -66.3 210.8 -33.2 227.2 9.5C243.5 52.1 257.1 104.2 228.5 121.1C200 138 129.3 119.7 84.2 138C39.1 156.3 19.5 211.2 -8.1 225.1C-35.7 239.1 -71.3 212.2 -109.1 189.7C-147 167.2 -186.9 149.1 -199.1 118.2C-211.3 87.3 -195.6 43.7 -190.3 3.1C-185 -37.5 -189.9 -75 -181.8 -113C-173.7 -150.9 -152.6 -189.3 -120.1 -195.1C-87.7 -200.8 -43.8 -173.9 3.4 -179.8C50.7 -185.8 101.3 -224.5 130.1 -216.6"
-                                    fill= {projectTitleColored ? "white" : "#4163e1"}
+                                    fill={
+                                        projectTitleColored
+                                            ? 'white'
+                                            : '#4163e1'
+                                    }
                                 ></path>
                             </g>
                         </svg>
@@ -141,7 +155,15 @@ export default React.memo(function Project(props) {
                 >
                     <div>
                         <div className={classes.StackTabContainer}>
-                            <div className={[classes.StackTab, !projectTitleColored && classes.StackTabColored].join(' ')}>Tech Stack</div>
+                            <div
+                                className={[
+                                    classes.StackTab,
+                                    !projectTitleColored &&
+                                        classes.StackTabColored,
+                                ].join(' ')}
+                            >
+                                Tech Stack
+                            </div>
                         </div>
 
                         <ul
@@ -156,12 +178,19 @@ export default React.memo(function Project(props) {
                 <div className={`${classes.Item} ${classes.LiveDemoItem}`}>
                     <div className={classes.BtnContainer}>
                         {/* PROJECT LIVE + GITHUB */}
-                        <CvButton type={'github'} url={props.projectData.github} colored={projectTitleColored}/>    
+                        <CvButton
+                            type={'github'}
+                            url={props.projectData.github}
+                            colored={projectTitleColored}
+                        />
 
                         {/* PROJECT LIVE DEMO */}
                         {/* CHECK LIVE DEMO LINK EXISTENCE */}
                         {props.projectData.liveDemo ? (
-                            <LiveIcon url={props.projectData.liveDemo} colored={projectTitleColored}/>
+                            <LiveIcon
+                                url={props.projectData.liveDemo}
+                                colored={projectTitleColored}
+                            />
                         ) : null}
                     </div>
                 </div>
